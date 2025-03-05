3506242fd25af9366392a5ddcf7740bc856cf4cf
raveendran.manickam <raveendran.manickam@ilink-systems.com>
2025-03-05 17:40:47 +0530

    second commit

--- src/main/java/CipherplusAPI/BaseService/BaseService.java
+++ src/main/java/CipherplusAPI/BaseService/BaseService.java
@@ -23,7 +23,7 @@ public class BaseService {
 
     public Response getRequest(String endpoint){
         return requestSpecification.contentType(ContentType.JSON)
-                .header("Authorization", "Bearer " + "")
+                .header("Authorization", "Bearer " + "YOUR_ACCESS_TOKEN") // Replace with a valid token or remove it
 
                 .get(endpoint);
     }
