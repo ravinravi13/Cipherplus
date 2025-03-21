e073de89ad59782e4d7c9089d6c8bcc87e00846d
raveendran.manickam <raveendran13102003@gmail.com>
2025-03-21 05:04:03 -0700

    Admin direct reportee testcase half completed

--- src/main/java/CipherplusAPI/BaseService/BaseService.java
+++ src/main/java/CipherplusAPI/BaseService/BaseService.java
@@ -23,7 +23,7 @@ public class BaseService {
 
     public Response getRequest(String endpoint) {
         return requestSpecification.contentType(ContentType.JSON)
-                .header("Authorization", "Bearer " + System.getenv("AZURE_ENTRA_TOKEN"))
+//                .header("Authorization", "Bearer " + System.getenv("AZURE_ENTRA_TOKEN"))
                 .get(endpoint);
     }
 
