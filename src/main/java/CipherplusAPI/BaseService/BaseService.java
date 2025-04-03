b6ae1310600ec950680d30a39c72fc03c6ee9b18
raveendran.manickam <raveendran13102003@gmail.com>
2025-04-03 19:01:02 +0530

    ManageGifts first testcase not completed

--- src/main/java/CipherplusAPI/BaseService/BaseService.java
+++ src/main/java/CipherplusAPI/BaseService/BaseService.java
@@ -23,7 +23,7 @@ public class BaseService {
 
     public Response getRequest(String endpoint){
         return requestSpecification.contentType(ContentType.JSON)
-                .header("Authorization", "Bearer " + "") // Replace with a valid token or remove it
+                .header("Authorization", "Bearer " + "token") // Replace with a valid token or remove it
 
                 .get(endpoint);
     }
